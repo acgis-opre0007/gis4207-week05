1e8b39c2e029a7a99c3a169c186a74448d061cc5
Maria T <113043144+acgis-torr0124@users.noreply.github.com>
2023-02-07 15:12:48 -0500

    adding describe 01

--- LauraO_MariaT/describe01.py
+++ LauraO_MariaT/describe01.py
@@ -4,4 +4,6 @@ import os
 file = r'..\..\..\..\data\Canada\province.shp'
 desc = arcpy.Describe(file)
 
-
+print("Basename" : {desc.name})
+print("Catalog" :{desc.catalogPath})
+print("Datatype": {desc.dataType}) 
