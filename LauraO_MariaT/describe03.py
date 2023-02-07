07f8cc21fc315414f7fb320b4370ca84389df696
Maria T <113043144+acgis-torr0124@users.noreply.github.com>
2023-02-07 16:21:03 -0500

    working on describe 03

--- LauraO_MariaT/describe03.py
+++ LauraO_MariaT/describe03.py
@@ -1,5 +1,4 @@
 import arcpy
-import os
 import sys
 
 def main():
@@ -11,11 +10,17 @@ def main():
         print("Error turn back")
 
 file = r'..\..\..\..\data\Canada\province.shp'
-desc = arcpy.da.Describe(file)
+desc_dic= arcpy.da.Describe(file)
 
-print(f'{"Basename":13}{desc.BaseName}')
-print(f'{"Catalog":13}{desc.catalogPath}')
-print(f'{"Datatype":13}{desc.dataType}')
+print(type(desc_dic))
+print(desc_dic.keys())
+print(desc_dic['baseName'])
+print(desc_dic['catalogPath'])
+print(desc_dic['dataType'])
+
+#print(f'{"Basename":13}{desc_dic.BaseName}')
+#print(f'{"Catalog":13}{desc_dic.catalogPath}')
+#print(f'{"Datatype":13}{desc_dic.dataType}')
 
 if __name__ == '__main__':
         main ()
