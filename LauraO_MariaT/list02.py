252872da877ea2d0295d8877e61e6b08e832e6be
Maria T <113043144+acgis-torr0124@users.noreply.github.com>
2023-02-09 12:21:53 -0500

    Missing sys.argv

---
+++ LauraO_MariaT/list02.py
@@ -0,0 +1,28 @@
+import sys
+
+def main():
+    global arcpy
+    
+    if len(sys.argv) !=2:
+        print ("Usage:List02.py <FeatureClassName>")
+        sys.exit()
+        
+    file = sys.argv[2]
+    
+    import arcpy
+    from arcpy import env
+
+
+    if not arcpy.Exists(file):
+        print("The feature class does not exist.")
+        sys.exit()
+            
+    arcpy.env.workspace = file
+    
+    des_FC = arcpy.ListFeatureClasses()
+    for fc in des_FC:
+         print(fc)    
+
+
+if __name__ == "__main__":
+        main ()
