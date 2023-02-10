7a6fbda14af2be3d29b6b1b6fed2c1f9731517fd
Maria T <113043144+acgis-torr0124@users.noreply.github.com>
2023-02-10 13:13:24 -0500

    Working on it list08

---
+++ LauraO_MariaT/list08.py
@@ -0,0 +1,33 @@
+import sys
+import os
+
+def main():
+    global arcpy 
+    
+    if len(sys.argv) !=2:
+        print ("Usage:List06.py <root_folder> <Point|Polyline|polygon> <out_file_name>")
+        sys.exit()
+    
+    rootFolder = sys.argv[1]
+    
+    import arcpy
+    
+    if not os.path.exists(rootFolder):
+        print ("The root_folder does not exist")
+        sys.exit()
+    
+    get_workspace_folder(rootFolder)     
+
+def get_workspace_folder(rootFolder):
+    for root, dirs, files in os.walk(rootFolder):
+        arcpy.env.workspace = root
+        workspace_list = arcpy.ListWorkspaces()
+        for workspace in workspace_list:
+            arcpy.env.workspace = workspace
+            fc_list = arcpy.ListFeatureClasses()
+            for fc in fc_list:
+                 print (os.path.abspath(workspace),fc)
+      
+        
+if __name__ == "__main__":
+        main ()
