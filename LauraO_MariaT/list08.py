d69f42ff69e2adaad740b20430995f2c1ea72f3b
Maria T <113043144+acgis-torr0124@users.noreply.github.com>
2023-02-10 15:10:03 -0500

    Working List08

--- LauraO_MariaT/list08.py
+++ LauraO_MariaT/list08.py
@@ -4,11 +4,13 @@ import os
 def main():
     global arcpy 
     
-    if len(sys.argv) !=2:
+    if len(sys.argv) !=4:
         print ("Usage:List06.py <root_folder> <Point|Polyline|polygon> <out_file_name>")
         sys.exit()
     
     rootFolder = sys.argv[1]
+    feature_type = sys.argv[2]
+    out_file = sys.argv[3]
     
     import arcpy
     
@@ -16,17 +18,27 @@ def main():
         print ("The root_folder does not exist")
         sys.exit()
     
-    get_workspace_folder(rootFolder)     
+    if not arcpy.da.Walk(rootFolder, datatype='FeatureClass'):
+        print ("The file does not contain a feature class")
+        sys.exit()
+    
+    get_workspace_folder(rootFolder, feature_type, out_file)     
 
-def get_workspace_folder(rootFolder):
-    for root, dirs, files in os.walk(rootFolder):
+def get_workspace_folder(rootFolder, feature_type, out_file):
+    for root, dirs, files in arcpy.da.walk(rootFolder, datatype='FeatureClass', type=['Point', 'Polyline', 'Polygon']):
         arcpy.env.workspace = root
         workspace_list = arcpy.ListWorkspaces()
         for workspace in workspace_list:
             arcpy.env.workspace = workspace
             fc_list = arcpy.ListFeatureClasses()
             for fc in fc_list:
+                if arcpy.Describe(fc).shapeType == feature_type:
                  print (os.path.abspath(workspace),fc)
+                 
+        with open(out_file, "w") as out_file:
+            out_file.write(f'{feature_type} feature classes in and below {rootFolder}')
+            for fc in fc_list:
+                out_file.write(fc)
       
         
 if __name__ == "__main__":
