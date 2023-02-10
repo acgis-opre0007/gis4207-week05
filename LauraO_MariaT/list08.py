767b20195167d6d138212cf7c917bb8d3504d5e8
Maria T <113043144+acgis-torr0124@users.noreply.github.com>
2023-02-10 16:44:02 -0500

    Added list08 and updated to good version

--- LauraO_MariaT/list08.py
+++ LauraO_MariaT/list08.py
@@ -5,14 +5,14 @@ def main():
     global arcpy 
     
     if len(sys.argv) !=4:
-        print ("Usage:List08.py <root_folder> <Point|Polyline|polygon> <out_file_name>")
+        print ("Usage:List08.py <root_folder> <Point|Polyline|Polygon> <out_file_name>")
         sys.exit()
     
     rootFolder = sys.argv[1]
     feature_type = sys.argv[2]
     out_file = sys.argv[3]
     
-    import arcpy
+    import arcpy.da
     
     if not os.path.exists(rootFolder):
         print ("The root_folder does not exist")
@@ -22,26 +22,32 @@ def main():
         print ("The file does not contain a feature class")
         sys.exit()
     
-    get_workspace_folder(rootFolder, feature_type, out_file)     
+    get_da_walker(rootFolder, feature_type, out_file)     
 
 #TO DO: not working at the moment
 
-def get_workspace_folder(rootFolder, feature_type, out_file):
-    for root, dirs, files in arcpy.da.walk(rootFolder, datatype='FeatureClass', type=['Point', 'Polyline', 'Polygon']):
-        arcpy.env.workspace = root
-        workspace_list = arcpy.ListWorkspaces()
-        for workspace in workspace_list:
-            arcpy.env.workspace = workspace
-            fc_list = arcpy.ListFeatureClasses()
-            for fc in fc_list:
-                if arcpy.Describe(fc).shapeType == feature_type:
-                 print (os.path.abspath(workspace),fc)
-                 
-        with open(out_file, "w") as out_file:
-            out_file.writer(f'{feature_type} feature classes in and below {rootFolder}')
-            for fc in fc_list:
-                out_file.write(fc)
+def get_da_walker(rootFolder, feature_type, out_file):
+    feat_class = []
+    walk = arcpy.da.Walk(rootFolder, datatype='FeatureClass', type= feature_type)
+
+    for dirpath, dirnames, filenames in walk:
+        for filename in filenames:
+            feat_class.append(os.path.join(dirpath, filename))  
+    
+    with open(out_file, "w") as file:
+            file.write(f'{feature_type} feature classes in and below {os.path.abspath(rootFolder)}')
+            for fc in feat_class:
+                file.write(fc)
       
+    #for root, dirs, files in arcpy.da.walk(rootFolder, datatype='FeatureClass', type=['Point', 'Polyline', 'Polygon']):
+        #arcpy.env.workspace = root
+        #workspace_list = arcpy.ListWorkspaces()
+        #for workspace in workspace_list:
+            #arcpy.env.workspace = workspace
+            #fc_list = arcpy.ListFeatureClasses()
+            #for fc in fc_list:
+                #if arcpy.Describe(fc).shapeType == feature_type:
+                    #print (os.path.abspath(workspace),fc, feature_type)
         
 if __name__ == "__main__":
         main ()
